f929987c79a4bbbaad5a810c371e774a409df1ba
Keith Davidson <keith.davidson@akersolutions.com>
2018-08-07 14:06:30 +0100

    Added Aurelia implementation, no service

---
+++ Aurelia/app-aurelia/src/components/user.ts
@@ -0,0 +1,64 @@
+export class User {    
+  name: string;
+  age: number;
+  email: string;
+  address: Address;
+  hobbies: string[];
+  posts: Post[];
+  isEdit:boolean = false;
+    
+  constructor(/*private dataService: DataService*/) {
+    console.log("constructor ran...")  
+  }
+
+  created(){
+    console.log("activate ran...")
+
+    this.name = "John Doe";
+    this.age = 30;
+    this.email = "test@test.com";
+    this.address = {
+      street: "50 Main street",
+      city: "Boston",
+      state: "MA"
+    }
+    this.hobbies = ["Write code", "Watch movies", "Listen to Movies"]
+  
+    //this.dataService.getPosts().then((posts) => {
+    //  this.posts = posts;
+    //});
+  }
+
+  onClick(){
+    this.name = "Keith Davidson";
+    this.hobbies.push("New Hobby");
+  }
+
+  addHobby(hobby){
+    this.hobbies.unshift(hobby);
+  }
+
+  deleteHobby(hobby){
+    for(let i = 0; i < this.hobbies.length;i++){
+      if(this.hobbies[i] == hobby){
+        this.hobbies.splice(i, 1);
+      }
+    }
+  }
+
+  toggleEdit(){
+    this.isEdit = !this.isEdit;
+  }
+}
+interface Address {
+  street: string,
+  city: string,
+  state: string
+}
+
+interface Post{
+  id: number,
+  title: string,
+  body: string,
+  userId: number
+}
